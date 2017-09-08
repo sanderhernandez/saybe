1c13d8a3b3cfb4b4d1d9b04d36caa4a10369f280
sanderhernandez <sanderhernandez@gmail.com>
2017-09-07 22:28:11 -0600

    Se eliminaron por innecesarias
    Se eliminaron por innecesarias.
    Y la "saybe/class_conexion" se elimino por que esta repetida dos veces

--- lab/registro.php
+++
@@ -1,9 +0,0 @@
-<?php include ('functions.php');
-$nombres=$_GET['nombres'];
-$telefono=$_GET['tel'];
-
-ejecutarSQLCommand("INSERT INTO  `usuarios` (nombres, telefono)
-VALUES ('$nombres', '$telefono')
-ON DUPLICATE KEY UPDATE `nombres`= '$nombres', `telefono`='$telefono';");
-
- ?>
