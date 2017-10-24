9d29f6f389c7eeffe6840355def54896d9279f3f
sanderhernandez <sanderhernandez@gmail.com>
2017-10-24 16:00:22 -0600

    Contra la injection sql -  algunas instruciones
    Eliminando la comilla simple (')

--- usuario/validar_usuario.php
+++ usuario/validar_usuario.php
@@ -1,7 +1,7 @@
 <?php
 	include('../conexion.php');
 	$user=$_GET["user"];
-	$contra=$_GET["contra"]; 
+	$contra=$_GET["contra"];
 
 	$sql = "SELECT
 			P.id_identidad,
@@ -12,10 +12,10 @@
 			P.apellido_segundo
 		FROM `tbl_usuario` as U left Join tbl_persona as P
 		on U.id_identidad = P.id_identidad
-		WHERE U.id_usuario='$user' and U.contrasena='$contra' and U.activo = 1";
+		WHERE U.id_usuario='" . str_replace("'","", $user) . "' and U.contrasena='" . str_replace("'","", $contra) . "' and U.activo = 1";
 
 	if($resultset=getSQLResultSet($sql)){
-		while ($row = $resultset->fetch_array(MYSQLI_NUM)){ 
+		while ($row = $resultset->fetch_array(MYSQLI_NUM)){
 			echo json_encode($row);
 		}
 	}else
